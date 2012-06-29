fca4e3456b44aa2d6ef325f3176b1e568f968705
Noel GUILBERT <noelguilbert@gmail.com>
2012-06-29 13:29:54 +0200

    Allow to pass an array of emails into the delivery_address configuration key.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -80,7 +80,16 @@ class Configuration implements ConfigurationInterface
                         ->scalarNode('sleep')->defaultValue(0)->end()
                     ->end()
                 ->end()
-                ->scalarNode('delivery_address')->end()
+                ->scalarNode('delivery_address')
+                    ->beforeNormalization()
+                        ->ifString()
+                        ->then(function($v) {
+                            return array($v);
+                        })
+
+                    ->end()
+                    ->prototype('scalar')->end()
+                ->end()
                 ->booleanNode('disable_delivery')->end()
                 ->booleanNode('logging')->defaultValue($this->debug)->end()
             ->end()
